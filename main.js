39b9ec1bfe67d651a1e4d6f918e5d04fa82553b5
Lakshman Peethani <lakshman_peethani@epam.com>
2017-02-18 13:33:09 +0530

    some minor change and setup

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const electron = require('electron')
-// Module to control application life.
+    // Module to control application life.
 const app = electron.app
-// Module to create native browser window.
+    // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-
+const ipc = electron.ipcMain
 const path = require('path')
 const url = require('url')
 
@@ -11,27 +11,55 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+const Menu = electron.Menu
+const Tray = electron.Tray
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+let appIcon = null
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+ipc.on('put-in-tray', function(event) {
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'icon-image-128.png'
+    const iconPath = path.join(__dirname, iconName)
+    appIcon = new Tray(iconPath)
+    const contextMenu = Menu.buildFromTemplate([{
+        label: 'Remove',
+        click: function() {
+            event.sender.send('tray-removed')
+        }
+    }, {
+        label: 'Demo',
+        click: function() {
+            event.sender.send('tray-removed')
+        }
+    }])
+    appIcon.setToolTip('Electron Demo in the tray.')
+    appIcon.setContextMenu(contextMenu)
+})
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+ipc.on('remove-tray', function() {
+    appIcon.destroy()
+})
+
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ width: 800, height: 600 })
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
 }
 
 // This method will be called when Electron has finished
@@ -40,20 +68,23 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function() {
+
+    if (appIcon) appIcon.destroy()
+
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
